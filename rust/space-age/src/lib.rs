8291ede43ebd9c809bb4d28146b121357256892b
Piers Powlesland <pierspowlesland@gmail.com>
2023-10-08 20:01:40 +0100

    Add space age solution

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,19 +1,17 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration(u64);
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+        Duration(s)
     }
 }
 
 pub trait Planet {
     fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+        d.0 as f64 / 31557600_f64 / Self::orbital_period()
     }
+    fn orbital_period() -> f64;
 }
 
 pub struct Mercury;
@@ -25,11 +23,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn orbital_period() -> f64 {
+        0.2408467
+    }
+}
+impl Planet for Venus {
+    fn orbital_period() -> f64 {
+        0.61519726
+    }
+}
+impl Planet for Earth {
+    fn orbital_period() -> f64 {
+        1.0
+    }
+}
+impl Planet for Mars {
+    fn orbital_period() -> f64 {
+        1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn orbital_period() -> f64 {
+        11.862615
+    }
+}
+impl Planet for Saturn {
+    fn orbital_period() -> f64 {
+        29.447498
+    }
+}
+impl Planet for Uranus {
+    fn orbital_period() -> f64 {
+        84.016846
+    }
+}
+impl Planet for Neptune {
+    fn orbital_period() -> f64 {
+        164.79132
+    }
+}
